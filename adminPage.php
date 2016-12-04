d51f26c4ac86b8ab058463304967ef85d5730c0a
Osamah Albayati <albayatiosamah@gmail.com>
2016-12-04 05:24:11 -0600

    Update adminPage.php

--- adminPage.php
+++ adminPage.php
@@ -1,24 +1,11 @@
 <?php
-
-$username = $_COOKIE['mycookie'];
-
-/* update the status of a member from pending to approved*/ 
-/* i think we need to add a new coloumn in member table where this coloumn shows date where the member signed with us and approve the member after one day of joining*/ 
-
-$sql = 'UPDATE member SET MStatusID=1 WHERE BookID = :bookid';
+include 'db.inc.php';
 
 /* adding new books to the library*/
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDBPDO";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    
+    $sql = "INSERT INTO book (ISBN, lastname, email)
     VALUES ('John', 'Doe', 'john@example.com')";
     // use exec() because no results are returned
     $conn->exec($sql);
@@ -37,3 +24,12 @@ $conn = null;
 
 echo "WElcome to adminPage!$username"
 ?>
+
+$username = $_COOKIE['mycookie'];
+
+/* update the status of a member from pending to approved*/ 
+/* i think we need to add a new coloumn in member table where this coloumn shows date where the member signed with us and approve the member after one day of joining*/ 
+
+$sql = 'UPDATE member SET MStatusID=1 WHERE BookID = :bookid';
+
+
