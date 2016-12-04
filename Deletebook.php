cd09f9fabc1060f3aa6b73f58ddb0bc89f0b85d3
Osamah Albayati <albayatiosamah@gmail.com>
2016-12-04 07:13:23 -0600

    Update Deletebook.php

--- Deletebook.php
+++ Deletebook.php
@@ -1,1 +1,17 @@
+try {
+ 
 
+    // sql to delete a record
+    $sql = "DELETE FROM book WHERE BStatusId=2";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Record deleted successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
